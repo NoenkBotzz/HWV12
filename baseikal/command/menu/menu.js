3b4322fce59bb333688a98b663e1832534a5ef3e
NoenkBotzz <firdausryan3113@gmail.com>
2023-01-11 01:26:10 +0700

    HWV12

---
+++ baseikal/command/menu/menu.js
@@ -0,0 +1,40 @@
+const menu =  ` 
+° Owner  : 𝚁𝚈𝙰𝙽🎭
+° Version : 12
+° Baileys : 4.4.0
+ ▰▱▰▱▰▱▰▱▰▱▰▱▰▱
+ ❍ BUG_MENU ❍
+┏━━⊱
+┣❏ Bugmenu
+┗━━⊱
+ ❍ ALL_MENU ❍
+┏━━⊱
+┣❏ Allmenu
+┗━━⊱
+❍ MENU_MODS ❍
+┏━━⊱
+┣❏ Tiktok
+┣❏ Methodeband
+┣❏ Afk
+┣❏ Ping
+┣❏ Owner
+┣❏ Listwibu
+┣❏ Nowa 628xxx
+┣❏ Verif 62xxx
+┣❏ User add 62xx
+┣❏ User del 62xxx
+┣❏ Setcmd [ reply sticker ]
+┣❏ Delcmd [ reply sticker ]
+┗━━⊱
+❍ MENU_GROUP ❍
+┏━━⊱
+┣❏ Antilink on / off
+┣❏ Welcome on / off
+┣❏ Detectadmin on / off
+┣❏ Kick 628xxx
+┣❏ Add 628xxx
+┣❏ Promote 628xx
+┣❏ Demote 628xx
+┗━━⊱
+©_𝚁𝚈𝙰𝙽 𝚃𝙷𝙴 𝙸𝙽𝚅𝙸𝚂𝙸𝙱𝙻𝙴
+exports.menu = menu
